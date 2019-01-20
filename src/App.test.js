847ad6f460875e4458a18f2bc048006367076e14
Heather Hartley <hlhartley@gmail.com>
2019-01-19 19:46:40 -0700

    Adds more tests for App.test

--- src/App.test.js
+++ src/App.test.js
@@ -1,24 +1,39 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import App from './App';
+  
+let wrapper;
+  
+describe('App', () => {
 
-it('matches the snapshot', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper).toMatchSnapshot()
-});
+beforeEach(() => {
+    wrapper = shallow(
+    <App />
+    );
+})
 
-// it('renders a thoughtList with the correct props', () => {
-//     const wrapper = shallow(<App />);
-//     const { thoughtList, deleteThought, editThought } = wrapper.find(ThoughtList).props();
-//     expect(thoughtList).toEqual([]);
-//     expect(deleteThought).toBeInstanceOf(Function);
-//     expect(editThought).toBeInstanceOf(Function);
-// });
+    it('matches the snapshot', () => {
+        expect(wrapper).toMatchSnapshot()
+    });
 
-it('initial state is an empty array', () => {
-});
+    it('renders a thoughtList with the correct props', () => {
+        const { thoughtList, searchInput, removeThought } = wrapper.find(ThoughtList).props();
+        expect(thoughtList).toEqual([]);
+        expect(searchInput).toEqual('');
+        // expect(removeThought).toBeInstanceOf(Function);
+    });
 
-it('createThought adds a thought to state', () => {
-});
+    it('should have a proper default state', () => {
+        expect(wrapper.state()).toEqual({
+            searchInput: "", thoughts: [],
+        })
+    })
 
+    it('createThought adds a thought to state', () => {
+        wrapper.instance().createThought()
+        expect(wrapper.state()).toEqual({
+            searchInput: "", thoughts: [{"id": 0}],
+        })
+    });
+})
 // Feel free to add additional tests. These are meant just to get you started.
